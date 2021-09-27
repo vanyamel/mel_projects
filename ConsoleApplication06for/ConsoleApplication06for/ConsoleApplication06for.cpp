a06297b1c0f0142a7093e0a1741de2ef18f9c787
vanyamel <90393911+vanyamel@users.noreply.github.com>
2021-09-27 23:39:06 +0300

    Add files via upload
    task_for

---
+++ ConsoleApplication06for/ConsoleApplication06for/ConsoleApplication06for.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void task_for01() {
+	int k, n;
+	cout << "k: ";
+	cin >> k;
+	cout << "n: ";
+	cin >> n;
+		int i;
+		for (i = 0; i < n; ++i)
+			cout << k << endl;
+}
+
+void task_for15() {
+	int a,n;
+	cout << "a: ";
+	cin >> a;
+	cout << "n: ";
+	cin >> n;
+	int i;
+	float result = 1;
+	for (i = 1; i <= n; ++i)
+		result *= a;
+	cout << result;
+}
+
+
+
+int main()
+{
+	task_for01();
+	task_for15();
+}
