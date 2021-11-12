68a6012b55f67d86f82c752ba3464ca99cef99e7
vanyamel <90393911+vanyamel@users.noreply.github.com>
2021-11-12 10:43:39 +0200

    Add files via upload

---
+++ structures/structures/structures.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Student {
+    char name[50];
+    int group;
+    int marks[5];
+};
+
+int main()
+{
+    Student students[10]{};
+
+    for (Student& student : students) {
+        cout << "Enter the student's name and group: ";
+        cin >> student.name >> student.group;
+
+        for (int& mark : student.marks) {
+            cout << "Enter mark: ";
+            cin >> mark;
+        }
+    }
+
+    bool found{ false };
+
+    for (Student& student : students) {
+        bool badMarkFound{ false };
+
+        for (int mark : student.marks) {
+            if (mark < 4) {
+                badMarkFound = true;
+                break;
+            }
+        }
+
+        if (!badMarkFound) {
+            found = true;
+            cout << student.name << ' ' << student.group << endl;
+        }
+    }
+
+    if (!found) {
+        cout << "There are no students with all marks above 4.";
+    }
+
+    return 0;
+}
